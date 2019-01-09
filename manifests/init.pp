ba83aada5b86d77c0690cfa4826c74e25603241b
John Graham <john.graham@virtua-enterprise.co.uk>
2019-01-09 20:29:37 +0000

    Adding class parameters and setting all variables to undef

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,47 @@
 # ============================================================
 # A class to manage the automounter services on a Linux client
 # ============================================================
-# @param autofs_enable [Boolean]
-# @param autofs_
-# @param autofs_
-# @param autofs_
-# @param autofs_
-# @param autofs_
-# @param autofs_
+# @param autofs_service_enable
+# @param autofs_service_ensure
+# @param autofs_config_test
+# @param autofs_config_usetls
+# @param autofs_config_tlsrequired
+# @param autofs_config_authrequired
+# @param autofs_config_authtype
+# @param autofs_config_user
+# @param autofs_config_secret
+# @param autofs_config_timeout
+# @param autofs_config_browse_mode
+# @param autofs_config_logging
+# @param autofs_config_map_object_class
+# @param autofs_config_entry_object_class
+# @param autofs_config_map_attribute
+# @param autofs_config_entry_attribute
+# @param autofs_config_value_attribute
+# @param autofs_config_auth_conf_file
+# @param autofs_config_use_misc_device
 
 class autofs (
 
-  $autofs_service_enable  = $::autofs::params::autofs_service_enable,
-  $autofs_service_ensure  = $::autofs::params::autofs_service_ensure,
-  $autofs_config_test     = $::autofs::params::autofs_config_test,
+  $autofs_service_enable            = undef,
+  $autofs_service_ensure            = undef,
+  $autofs_config_test               = undef,
+  $autofs_config_usetls             = undef,
+  $autofs_config_tlsrequired        = undef,
+  $autofs_config_authrequired       = undef,
+  $autofs_config_authtype           = undef,
+  $autofs_config_user               = undef,
+  $autofs_config_secret             = undef,
+  $autofs_config_timeout            = undef,
+  $autofs_config_browse_mode        = undef,
+  $autofs_config_logging            = undef,
+  $autofs_config_map_object_class   = undef,
+  $autofs_config_entry_object_class = undef,
+  $autofs_config_map_attribute      = undef,
+  $autofs_config_entry_attribute    = undef,
+  $autofs_config_value_attribute    = undef,
+  $autofs_config_auth_conf_file     = undef,
+  $autofs_config_use_misc_device    = undef,
 
 ) inherits autofs::params {
 
@@ -21,6 +49,4 @@ class autofs (
   include autofs::config
   include autofs::service
 
-  validate_re($autofs_service_ensure, [ '^running$', '^stopped$' ])
-  validate_re($autofs_config_test, [ '^hello$' ])
 }
