aa0e259c706ddb439e59e39c30eab99cf369b70c
John Graham <john.graham@virtua-enterprise.co.uk>
2019-01-13 22:09:11 +0000

    Added test

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -6,8 +6,64 @@ describe 'autofs::config' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
-
       it { is_expected.to contain_class('autofs::config') }
+
+      it do
+        is_expected.to contain_file('/etc/sysconfig').with(
+          'ensure' => 'directory',
+          'owner' => 'root',
+          'group' => 'root',
+          'mode' => '0755',
+        )
+      end
+
+      it do
+        is_expected.to contain_file('/etc/auto_home').only_with(
+          'ensure' => 'absent',
+        )
+      end
+
+      it do
+        is_expected.to contain_file('/etc/auto_share').only_with(
+          'ensure' => 'absent',
+        )
+      end
+
+      it do
+        is_expected.to contain_file('/etc/auto.home').only_with(
+          'ensure' => 'absent',
+        )
+      end
+
+      it do
+        is_expected.to contain_file('/etc/auto.share').only_with(
+          'ensure' => 'absent',
+        )
+      end
+
+      it do
+        is_expected.to contain_file('/etc/autofs_ldap_auth.conf').with(
+          'ensure' => 'file',
+          'path' => '/etc/autofs_ldap_auth.conf',
+          'owner' => 'root',
+          'group' => 'root',
+        )
+      end
+
+      it { is_expected.to contain_file('/etc/autofs_ldap_auth.conf').that_requires(['Package[autofs]']) }
+
+      it do
+        is_expected.to contain_file('/etc/sysconfig/autofs').with(
+          'ensure' => 'file',
+          'path' => '/etc/sysconfig/autofs',
+          'owner' => 'root',
+          'group' => 'root',
+          'mode' => '0600',
+        )
+      end
+
+      it { is_expected.to contain_file('/etc/sysconfig/autofs').that_requires(['File[/etc/sysconfig]', 'Package[autofs]']) }
+
     end
   end
 end
